79b8f4d3cbe2dcee94e37f5e6b52d840453cb392
lshkn2000 <48271691+lshkn2000@users.noreply.github.com>
2022-02-03 10:56:08 +0900

    도착율 기록 추가

--- path_planning_simulator/run_simulation.py
+++ path_planning_simulator/run_simulation.py
@@ -1,5 +1,6 @@
 # from IPython.display import clear_output
 # %matplotlib notebook
+import os
 import random
 import time
 import datetime
@@ -22,6 +23,15 @@ from policy.sac import SAC
 from policy.td3_new import TD3
 from utils.plot_graph import plot_data
 
+# from utils.pretrained import PretrainedSim
+
+
+def make_directories(path):
+    try:
+        os.makedirs(path)
+    except OSError:
+        if not os.path.isdir(path):
+            raise
 
 def run_sim(env, max_episodes=1, max_step_per_episode=50, render=True, seed_num=1, n_warmup_batches=5, update_target_interval=1, **kwargs):
     # simulation start
@@ -38,6 +48,11 @@ def run_sim(env, max_episodes=1, max_step_per_episode=50, render=True, seed_num=
 
     total_seeds_episodes_results = []
     plot_log_data = SummaryWriter()
+
+    total_collision = 0
+    total_goal = 0
+    total_time_out = 0
+
     for i_seed, seed in enumerate(SEED):
         torch.manual_seed(seed)
         np.random.seed(seed)
@@ -45,14 +60,14 @@ def run_sim(env, max_episodes=1, max_step_per_episode=50, render=True, seed_num=
 
         episodes_result = []
 
-        for i_episode in range(max_episodes+1):
+        for i_episode in range(1, max_episodes+1):
             state = env.reset(random_position=False, random_goal=False, max_steps=max_step_per_episode)
             is_terminal = False
             score = 0.0
             time_step_for_ep = 0
 
             n_warmup_batches = n_warmup_batches
-            update_target_interval = update_target_interval # 1 for DDPG
+            update_target_interval = update_target_interval # 1 for DDP
 
             for t in range(1, max_step_per_episode+1):
                 if i_episode > 50:
@@ -66,9 +81,9 @@ def run_sim(env, max_episodes=1, max_step_per_episode=50, render=True, seed_num=
                         action = np.random.randn(action_space).clip(-max_action_scale, max_action_scale)
                 new_state, reward, is_terminal, info = env.step(action)
                 if env.robot.is_discrete_actions:
-                    env.robot.policy.store_trajectory(state, discrete_action_index, reward/10, new_state, is_terminal)
+                    env.robot.store_trjectory(state, discrete_action_index, reward/10, new_state, is_terminal)
                 else:
-                    env.robot.policy.store_trajectory(state, action, reward/10, new_state, is_terminal)
+                    env.robot.store_trjectory(state, action, reward/10, new_state, is_terminal)
 
                 state = new_state
                 time_step_for_ep += 1
@@ -85,6 +100,16 @@ def run_sim(env, max_episodes=1, max_step_per_episode=50, render=True, seed_num=
 
                 if is_terminal or t == max_step_per_episode:
                     print("{} seeds {} episode, {} steps, {} reward".format(i_seed, i_episode, time_step_for_ep, score))
+                    if info == 'Goal':
+                        total_goal += 1
+                    elif info == 'Collision' or info == 'OutBoundary':
+                        total_collision += 1
+                    elif info == 'TimeOut':
+                        total_time_out += 1
+
+                    print(
+                        "Total Episode : {:5} , Total Collision : {:5f} , Total Goal : {:5f} , Total Time Out : {:5f}, Success Rate : {:.4f}".format(
+                            i_episode, total_collision, total_goal, total_time_out, total_goal / i_episode))
                     gc.collect()
                     break
 
@@ -99,7 +124,7 @@ def run_sim(env, max_episodes=1, max_step_per_episode=50, render=True, seed_num=
                 env.robot.policy.save("learning_data/tmp")
 
             # render check
-            if render and i_episode % 5 == 0 and i_episode != 0:
+            if render and i_episode % 50 == 0 and i_episode != 0 and time_step_for_ep < 120:
                 env.render(path_info=True, is_plot=False)
 
         total_seeds_episodes_results.append(episodes_result)
@@ -117,6 +142,9 @@ def run_sim(env, max_episodes=1, max_step_per_episode=50, render=True, seed_num=
 
 
 if __name__ == "__main__":
+    make_directories("learning_data/reward_graph")
+    make_directories("learning_data/video")
+
     # 환경 소환
     env = Environment(start_rvo2=True)
 
@@ -132,13 +160,13 @@ if __name__ == "__main__":
     # 1. 행동이 이산적인지 연속적인지 선택
     # 2. 로봇 초기화
     is_discrete_action_space = None # continuous action space 이면 None
-    robot = Robot(discrete_action_space=is_discrete_action_space, is_holomonic=False, robot_name="Robot")
+    robot = Robot(discrete_action_space=is_discrete_action_space, is_holomonic=True, robot_name="Robot")
     # robot_init_position = {"px":0, "py":-2, "vx":0, "vy":0, "gx":0, "gy":4, "radius":0.2}
     robot.set_agent_attribute(px=0, py=-2, vx=0, vy=0, gx=0, gy=4, radius=0.2, v_pref=1, time_step=time_step)
 
     # 장애물 소환
     # 3. 동적 장애물
-    dy_obstacle_num = 0
+    dy_obstacle_num = 5
     dy_obstacles = [None] * dy_obstacle_num
     for i in range(dy_obstacle_num):
         dy_obstacle = DynamicObstacle()
@@ -167,6 +195,7 @@ if __name__ == "__main__":
 
     # 5. 로봇 정책(행동 규칙) 세팅
     observation_space = 7 + (dy_obstacle_num * 5) + (st_obstacle_num * 4) # robot state(x, y, vx, vy, gx, gy, radius) + dy_obt(x,y,vx,vy,r) + st_obt(x,y,width, height)
+    # observation_space = 17
     # 로봇의 action space 설정
     action_space = 2    # 이산적이라면 상,하,좌,우, 대각선 방향 총 8가지
     max_action_scale = 1    # 가속 테스트용이 아니라면 스케일은 1로 고정하는 것을 추천. 속도 정보를 바꾸려면 로봇 action 에서 직접 바꾸는 방식이 좋을 듯 하다.
