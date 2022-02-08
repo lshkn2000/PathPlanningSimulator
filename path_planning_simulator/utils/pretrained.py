87daa26c0bb667655b390c1b792bffca684624e9
lshkn2000 <48271691+lshkn2000@users.noreply.github.com>
2022-02-08 18:09:02 +0900

    Update pretrained.py

--- path_planning_simulator/utils/pretrained.py
+++ path_planning_simulator/utils/pretrained.py
@@ -1,6 +1,11 @@
 import collections
 import itertools
+import random
+
 import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib.patches import Circle, Rectangle, ConnectionPatch
+import matplotlib.lines as mlines
 import torch
 import rvo2
 
@@ -9,26 +14,32 @@ device = "cuda:0" if torch.cuda.is_available() else "cpu"
 
 
 class PretrainedSim(object):
-    def __init__(self, env, time_step, robot_with_dy_obstacles_list, max_step=200, max_speed=1):
+    def __init__(self, env, time_step, max_step=200, max_speed=1):
         self.sim = None
         self.robot_position = None
         self.target_norm = None
+        self.action_noise = 0.1
 
-        self.robot = robot_with_dy_obstacles_list[0]
-        self.dy_obstacles_list = robot_with_dy_obstacles_list[1:]
-        self.robot_with_dy_obstacles_list = robot_with_dy_obstacles_list
+        self.env = env
+
+        self.robot = self.env.robot
+        self.dy_obstacles_list = self.env.dy_obstacles_list
+        self.robot_with_dy_obstacles_list = [self.robot] + self.dy_obstacles_list
 
         self.max_steps = max_step
         self.max_speed = max_speed
-        self.env = env
         self.time_step = time_step
         self.params = {"neighborDist": 10, "maxNeighbors": 20, "timeHorizon": 5, "timeHorizonObst": 5}
 
         self.pretraied_replay_buffer = collections.deque(maxlen=100000)
 
-    def reset(self):
+    def reset(self, random_robot_spawn=True):
         self.env.reset()
         self.dy_obstacles_list = self.env.dy_obstacles_list
+        if random_robot_spawn:
+            self.robot.px, self.robot.py = random.uniform(-4, 4), random.uniform(-5, -3)
+        else:
+            self.robot = self.env.robot
         self.robot_with_dy_obstacles_list = [self.robot] + self.dy_obstacles_list
         self.target_norm = None
 
@@ -50,20 +61,21 @@ class PretrainedSim(object):
         if self.target_norm is None:
             self.target_norm = target_norm
 
-        delta_reward = lambda x: 5 * np.tanh(x) if x > 0 else np.tanh(0.9 * x)
-
+        # 목적지로의 이동 변화가 많을 수록 큰 보상
+        delta_reward = lambda x: np.tanh(x) if x > 0 else np.tanh(0.9 * x)
         reward += delta_reward(self.target_norm - target_norm)
 
         self.target_norm = target_norm
 
         reach_goal = np.linalg.norm(
-            robot_position - np.array(self.robot.goal)) < robot_radius + 0.1  # offset
+            robot_position - np.array(self.robot.goal)) < self.robot.radius + self.robot.goal_offset  # offset
 
         # 2. reward for terminal
         if reach_goal:
             reward += 10
             done = True
             info = "Goal"
+            print('goal!')
             self.target_norm = None
 
         else:
@@ -137,8 +149,6 @@ class PretrainedSim(object):
             self.sim.setAgentPrefVelocity(agent, tuple(pref_velocity))
 
     def pretrain(self):
-        check_dy_obstacles_reach_goal = [0] * len(self.robot_with_dy_obstacles_list)  # rvo2의 목적지 도달 확인용
-        check_reach_goal_pose = [0] * len(self.robot_with_dy_obstacles_list)  # rvo2의 목적지 도달 위치 기록용
         robot_reached = False
 
         # logged robot and dy-obj trajectories
@@ -146,8 +156,11 @@ class PretrainedSim(object):
             # change obstacles position (=reset)
             self.pretrain_()
 
+            check_dy_obstacles_reach_goal = [0] * len(self.robot_with_dy_obstacles_list)  # rvo2의 목적지 도달 확인용
+            check_reach_goal_pose = [0] * len(self.robot_with_dy_obstacles_list)  # rvo2의 목적지 도달 위치 기록용
+
             #  loop until logged trajectory of robot reached goal
-            self.dy_obstacles_states = [[] for _ in range(len(self.robot_with_dy_obstacles_list))]
+            self.robot_n_dy_obstacles_states = [[] for _ in range(len(self.robot_with_dy_obstacles_list))]
 
             for step in range(self.max_steps):
                 self.sim.doStep()
@@ -163,7 +176,14 @@ class PretrainedSim(object):
                         rvo2_dy_obstacle_velocity = self.sim.getAgentVelocity(i)
                         dy_obstacle_goal = agent.goal
                         reach_goal = np.linalg.norm(
-                            np.array(rvo2_dy_obstacle_pose) - np.array(dy_obstacle_goal)) < agent.radius + 0.1 # offset
+                            np.array(rvo2_dy_obstacle_pose) - np.array(dy_obstacle_goal)) < agent.radius + self.robot.goal_offset   # offset
+
+                        if i == 0:  # robot state : px py vx vy gx gy r
+                            robot_goal_vec = (agent.goal[0] - rvo2_dy_obstacle_pose[0], agent.goal[1] - rvo2_dy_obstacle_pose[1])
+                            self.robot_n_dy_obstacles_states[i].append(
+                                rvo2_dy_obstacle_pose + rvo2_dy_obstacle_velocity + robot_goal_vec + (agent.radius,))
+                        else:       # obstacle state : px py vx vy r
+                            self.robot_n_dy_obstacles_states[i].append(rvo2_dy_obstacle_pose + rvo2_dy_obstacle_velocity + (agent.radius,))
 
                         if reach_goal:
                             check_dy_obstacles_reach_goal[i] = reach_goal
@@ -172,40 +192,58 @@ class PretrainedSim(object):
                             if i == 0:
                                 robot_reached = True
 
-                        if i == 0:  # robot state : px py vx vy gx gy r
-                            self.dy_obstacles_states[i].append(
-                                rvo2_dy_obstacle_pose + rvo2_dy_obstacle_velocity + agent.goal + (agent.radius,))
-                        else:       # obstacle state : px py vx vy r
-                            self.dy_obstacles_states[i].append(rvo2_dy_obstacle_pose + rvo2_dy_obstacle_velocity + (agent.radius,))
-
                     # 목적지 도달하면 그 자리에 멈춤
                     else:
                         self.sim.setAgentVelocity(i, (0, 0))
-                        self.dy_obstacles_states[i].append(check_reach_goal_pose[i] + (0, 0) + (agent.radius, ))
+                        self.robot_n_dy_obstacles_states[i].append(check_reach_goal_pose[i] + (0, 0) + (agent.radius, ))
 
         # obstacles state : [px, py, vx, vy, radius]
-        obstacles_states = self.dy_obstacles_states
+        obstacles_states = self.robot_n_dy_obstacles_states
 
         # slicing obstacles state according to number of obstacles
         obstacles_states = np.array(obstacles_states, dtype=object)
         sliced_agents_states = np.array([obstacles_states[:,i] for i in range(len(obstacles_states[0]))])
 
-        for i, state in enumerate(sliced_agents_states[:-1]):
+        for i, ob in enumerate(sliced_agents_states[1:-1]):
+            robot_ob = ob[0]
+            # 위치, 속도를 로봇 기준 상대 좌표로 변환
+            dy_obstacle_ob = [(dy_obstacle[0] - robot_ob[0], dy_obstacle[1] - robot_ob[1],
+                               dy_obstacle[2] - robot_ob[2], dy_obstacle[3] - robot_ob[3],
+                               dy_obstacle[4]) for dy_obstacle in ob[1:]]
+
+            # 거리순으로 정렬 (먼 순으로)
+            dy_obstacle_ob.sort(key=lambda x: (pow(x[0], 2) + pow(x[1], 2)) ** 0.5, reverse=True)
+            # state
+            state = [robot_ob] + dy_obstacle_ob
+
+            # reward
             reward, is_terminal = self.reward_function(state)
-            new_state = sliced_agents_states[i+1]
-            action = sliced_agents_states[i+1][0][2:4]
+
+            # new_state
+            new_ob = sliced_agents_states[i+1]
+            new_robot_ob = new_ob[0]
+            new_dy_obstacle_ob = [(dy_obstacle[0] - new_robot_ob[0], dy_obstacle[1] - new_robot_ob[1],
+                                   dy_obstacle[2] - new_robot_ob[2], dy_obstacle[3] - new_robot_ob[3],
+                                   dy_obstacle[4]) for dy_obstacle in new_ob[1:]]
+            new_state = [new_robot_ob] + new_dy_obstacle_ob
+
+            # action
+            action = new_robot_ob[2:4]
+            action = np.array(action)
+            action += np.random.normal(0.0, 1 * 0.1, size=2)
+            action = action.clip(-1, 1)
 
             # flatten state data
-            state = self.flatten_(state)
-            new_state = self.flatten_(new_state)
+            state = np.array(self.flatten_(state))
+            new_state = np.array(self.flatten_(new_state))
 
+            # store trajectory for training
             self.env.robot.policy.store_trajectory(state, action, reward, new_state, is_terminal)
 
             self.pretraied_replay_buffer.append((state, action, reward, new_state, is_terminal))
 
         if len(self.env.robot.policy.replay_buffer) > self.env.robot.policy.replay_buffer.batch_size:
             self.env.robot.policy.train()
-            self.env.robot.policy.update_network()
 
     def save_model(self):
         self.env.robot.policy.save("learning_data/tmp")
@@ -213,5 +251,49 @@ class PretrainedSim(object):
     def get_pretrained_replay_buffer(self):
         return self.pretraied_replay_buffer
 
+    def render(self, obstacles_states):
+        robot_states = obstacles_states[0]
+        dy_obstacles_states = obstacles_states[1:]
 
+        # color setting
+        robot_color = 'chartreuse'
+        static_obstacle_color = 'yellow'
+        dynamic_obstacle_color = 'aqua'
+        goal_color = 'red'
+
+        fig, ax = plt.subplots(figsize=(self.env.square_width, self.env.square_height))
+        ax.set_xlim(-int(self.env.square_width / 2), int(self.env.square_width / 2))
+        ax.set_ylim(-int(self.env.square_width / 2), int(self.env.square_width / 2))
+        ax.set_xlabel('x(m)', fontsize=16)
+        ax.set_ylabel('y(m)', fontsize=16)
+
+        # 에피소드, 스텝 시간 표시
+        step_cnt = plt.text(self.env.square_width / 2, self.env.square_height / 2, 'Step : {}'.format(0), fontsize=16)
+        ax.add_artist(step_cnt)
+
+        # 목적지 그리기
+        goal_x, goal_y = self.robot.goal
+        goal = mlines.Line2D([goal_x], [goal_y], color=goal_color, marker='*', linestyle='None', markersize=15,
+                             label='Goal')
+        ax.add_artist(goal)
+        # 도착지 허용범위 그리기
+        goal_offset = Circle((goal_x, goal_y), self.robot.goal_offset, fill=True, color='salmon')
+        ax.add_artist(goal_offset)
+
+        # j개의 동적 장애물 이동 경로 그리기
+        dy_obstacle_circle_list = []
+        for j, dy_obstacle in enumerate(dy_obstacles_states):
+            for k, dy_obstacle_state in enumerate(dy_obstacle):
+                if k % 2 == 0:
+                    dy_obstacle_circle = Circle(dy_obstacle_state[:2], dy_obstacle_state[4], fill=True, color=dynamic_obstacle_color, ec='black')
+                    plt.text(dy_obstacle_state[0], dy_obstacle_state[1], "{}".format(k), fontsize=13, ha='center', fontweight='bold')
+                    ax.add_artist(dy_obstacle_circle)
+
+        for i, robot_state in enumerate(robot_states):
+            if i % 2 == 0:
+                robot_circle = Circle(robot_state[:2], robot_state[6], fill=True, color=robot_color, ec='black')
+                plt.text(robot_state[0], robot_state[1], "{}".format(i), fontsize=10, ha='center', fontweight='bold')
+                ax.add_artist(robot_circle)
+
+        plt.show()
 
